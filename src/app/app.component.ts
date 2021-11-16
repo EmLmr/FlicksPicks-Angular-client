38a6050111333a84350d8852d0d10e8aa4b63ee4
EmLmr <emeline.coding@gmail.com>
2021-11-16 17:25:49 +0100

    created movie-card component
    movie card only shows movie title and image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -14,17 +15,23 @@ export class AppComponent {
   title = 'FlicksPicks-Angular-client';
 
   constructor(public dialog: MatDialog) {}
-  //will open the dialog when the signup button is clicked
+  //will open the dialog when the "Sign up" button is clicked
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
       //dialog width
       width: '280px',
     });
   }
-  //will open the dialog when the sign up button is clicked
+  //will open the dialog when the "Login" button is clicked
   openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px',
+    });
+  }
+  //will show all movies when the "All Movies" button is clicked
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px',
     });
   }
 }
